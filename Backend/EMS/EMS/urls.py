4eddb53e3aa2669a07871d701bb79763d092f9dc
vaibhavraut07 <vaibhavraut087@gmail.com>
2025-01-09 16:09:33 +0530

    Rename token endpoint to login for clarity

--- Backend/EMS/EMS/urls.py
+++ Backend/EMS/EMS/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('api/', include('employees.urls')),
 
     # JWT authentication endpoints
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/', TokenObtainPairView.as_view(), name='login'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
